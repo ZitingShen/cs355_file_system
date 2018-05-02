d949c9085bb40e5f43c98404fff0c83709ed66a5
Ziting Shen <zitingshenbmc@gmail.com>
2018-05-02 13:34:10 -0400

    continue working on format

--- fs/format.c
+++ fs/format.c
@@ -1,4 +1,4 @@
-#include "defrag.h"
+#include "util.h"
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,16 +6,14 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <time.h>
 #include <errno.h>
 
-#define FILE_NAME	"large-frag"
-#define	MULTIPLIER	1.1
-#define OUTPUT_SIZE (int) (FILE_SIZE*MULTIPLIER)
 #define BLOCK_SIZE 	512
 #define POINTER_N	BLOCK_SIZE/sizeof(int)
-#define	FILE_SIZE 	(POINTER_N*POINTER_N*10 + POINTER_N*N_IBLOCKS + N_DBLOCKS + 1)*BLOCK_SIZE
 
 #define BOOT 		'b'
+#define INODE_RATIO	1/100
 
 void write_triple_indirect_block(int block, struct superblock *sb, int *num_block);
 void write_double_indirect_block(int block, struct superblock *sb, int *num_block);
@@ -26,31 +24,63 @@ void *disk;
 int ind = 0;
 int next = 0;
 
+char *file_name = "";
+int file_size = 1024*1024;
+
 int main(int argc, char **argv) {
-	disk = malloc(OUTPUT_SIZE);
+	int fd, out;
+
+	if(argc != 2 && argc != 4) {
+		printf("usage: format <filename> [-s <file size>]\n");
+		exit(-1);
+	}
+	parse(argc, argv);
+
+	fd = open(file_name, O_RDWR | O_CREAT, 0666);
+	ftruncate(fd, file_size);
+
+	disk = malloc(OFFSET_SUPERBLOCK - OFFSET_BOOT);
 	if(!disk) {
-		printf("%s: %s\n", "Fail to malloc space for the disk image:", strerror(errno));
+		printf("%s: %s\n", "Fail to malloc space for the disk image", strerror(errno));
 		return -1;
 	}
-	bzero(disk, OUTPUT_SIZE);
 	memset(disk, BOOT, OFFSET_SUPERBLOCK - OFFSET_BOOT);
+	out = write(fd, disk, OFFSET_SUPERBLOCK - OFFSET_BOOT);
+	if(out != OFFSET_SUPERBLOCK - OFFSET_BOOT) {
+		printf("%s: %s\n", "Fail to write disk image", strerror(errno));
+		return -1;
+	}
+	free(disk);
 
 	struct superblock sb;
 	sb.size = BLOCK_SIZE;
 	sb.inode_offset = 0;
-	sb.data_offset = 1;
+	sb.data_offset = (file_size*INODE_RATIO)/BLOCK_SIZE;
+	sb.swap_offset = (file_size - OFFSET_START)/BLOCK_SIZE;
 	sb.free_inode = 1;
+	sb.free_block = (file_size - OFFSET_START)/BLOCK_SIZE - 1;
 
+	out = write(fd, &sb, sizeof(struct superblock));
+	if(out != sizeof(struct superblock)) {
+		printf("%s: %s\n", "Fail to write disk image", strerror(errno));
+		return -1;
+	}
+
+	// write root directory
 	struct inode node;
 	node.next_inode = 0;
 	node.protect = 0;
+
+	node.parent = -1;
+	node.permission = 7;
+	node.type = 'd';
 	node.nlink = 1;
-	node.size = FILE_SIZE;
-	node.uid = 1;
-	node.gid = 1;
-	node.ctime = 1;
-	node.mtime = 1;
-	node.atime = 1;
+	node.size = 0;
+	node.uid = 0;
+	node.gid = 0;
+	node.ctime = time(0);
+	node.mtime = time(0);
+	node.atime = time(0);
 	for(int i = 0; i < N_DBLOCKS; i++)
 		node.dblocks[i] = 0;
 	for(int i = 0; i < N_IBLOCKS; i++)
@@ -58,42 +88,33 @@ int main(int argc, char **argv) {
 	node.i2block = 0;
 	node.i3block = 0;
 
-	int num_block = FILE_SIZE/BLOCK_SIZE;
-	if(FILE_SIZE % BLOCK_SIZE != 0)
-		num_block++;
-
-	if(num_block > 0) {
-		node.i2block = next;
-		next++;
-		write_double_indirect_block(node.i2block, &sb, &num_block);
+	out = write(fd, &node, sizeof(struct inode));
+	if(out != sizeof(struct inode)) {
+		printf("%s: %s\n", "Fail to write disk image", strerror(errno));
+		return -1;
 	}
 
-	int i = N_IBLOCKS - 1;
-	while(num_block > 0 && i >= 0) {
-		node.iblocks[i] = next;
-		next++;
-		write_single_indirect_block(node.iblocks[i], &sb, &num_block);
-		i--;
+	// chain free inodes
+	node.nlink = 0;
+	int node_index = sb.free_inode;
+	int itr;
+	for(itr = sb.inode_offset*BLOCK_SIZE; 
+		itr <= sb.data_offset*BLOCK_SIZE;
+		itr += sizeof(struct inode)) {
+		node_index++;
+		node.next_inode = node_index;
+
+		out = write(fd, &node, sizeof(struct inode));
+		if(out != sizeof(struct inode)) {
+			printf("%s: %s\n", "Fail to write disk image", strerror(errno));
+			return -1;
+		}
 	}
 
-
-	i = N_DBLOCKS - 1;
-	while(num_block > 0 && i >= 0) {
-		node.dblocks[i] = next;
-		next++;
-		write_direct_block(node.dblocks[i], &sb);
-		num_block--;
-		i--;
-	}
-
-	if(num_block > 0) {
-		node.i3block = next;
-		next++;
-		write_triple_indirect_block(node.i3block, &sb, &num_block);
-	}
-
-	sb.free_block = next + sb.data_offset;
-	sb.swap_offset = next + sb.data_offset;
+	// write free data block lists
+	int free_current = OFFSET_START + sb.free_inode*BLOCK_SIZE;
+	int counter = 0;
+	lseek(fd, offset, SEEK_SET);
 
 	int *end = (int *) (disk + OFFSET_START + (sb.data_offset + next)*BLOCK_SIZE);
 	*end = -1;
@@ -111,42 +132,29 @@ int main(int argc, char **argv) {
 	free(disk);
 }
 
-void write_triple_indirect_block(int block, struct superblock *sb, int *num_block) {
-	int *itr = (int *) (disk + OFFSET_START + (sb->data_offset + block)*BLOCK_SIZE);
-	for(int i = 0; i < BLOCK_SIZE/sizeof(int) && *num_block > 0; i++) {
-		*itr = next;
-		next++;
-		write_double_indirect_block(*itr, sb, num_block);
-		itr++;
-	}
-}
-
-void write_double_indirect_block(int block, struct superblock *sb, int *num_block) {
-	int *itr = (int *) (disk + OFFSET_START + (sb->data_offset + block)*BLOCK_SIZE);
-	for(int i = 0; i < BLOCK_SIZE/sizeof(int)&& *num_block > 0; i++) {
-		*itr = next;
-		next++;
-		write_single_indirect_block(*itr, sb, num_block);
-		itr++;
-	}
-}
-
-void write_single_indirect_block(int block, struct superblock *sb, int *num_block) {
-	int *itr = (int *) (disk + OFFSET_START + (sb->data_offset + block)*BLOCK_SIZE);
-	for(int i = 0; i < BLOCK_SIZE/sizeof(int)&& *num_block > 0; i++) {
-		*itr = next;
-		next++;
-		write_direct_block(*itr, sb);
-		(*num_block)--;
-		itr++;
-	}
-}
-
-void write_direct_block(int block, struct superblock *sb) {
-	int *loc = (int *) (disk + OFFSET_START + (sb->data_offset + block)*BLOCK_SIZE);
-	for(int i = 0; i < BLOCK_SIZE/sizeof(int); i++) {
-		*loc = ind;
-		loc++; 
-		ind++;
+void parse(int argc, char **argv) {
+	if (argc == 2) {
+		file_name = argv[1];
+	} else {
+		if (strcmp(argv[1], "-s") == 0) {
+			int mb = atoi(argv[2]);
+			if(mb <= 0) {
+				printf("error: invalid file size\n");
+				exit(-1);
+			}
+			file_size *= mb;
+			file_name = argv[3];
+		} else if (strcmp(argv[2], "-s") == 0) {
+			int mb = atoi(argv[3]);
+			if(mb <= 0) {
+				printf("error: invalid file size\n");
+				exit(-1);
+			}
+			file_size *= mb;
+			file_name = argv[1];
+		} else {
+			printf("usage: format <filename> [-s <file size>]\n");
+			exit(-1);
+		}
 	}
 }
--- fs/util.h
+++ fs/util.h
@@ -1,3 +1,15 @@
+#ifndef __UTIL__
+#define __UTIL__
+
+#define	POINTER_SIZE		sizeof(int)
+
+#define OFFSET_BOOT			0
+#define OFFSET_SUPERBLOCK	512
+#define	OFFSET_START		1024
+
+#define N_DBLOCKS 			10
+#define N_IBLOCKS 			4
+
 struct superblock {
 	int size; /* size of blocks in bytes */
 	int inode_offset; /* offset of inode region in blocks */
@@ -5,20 +17,30 @@ struct superblock {
 	int swap_offset; /* swap region offset in blocks */
 	int free_inode; /* head of free inode list, index */
 	int free_block; /* head of free block list, index */
-	int root_inode; /* pointer to inode of the root directory */
 };
 
 struct inode {
 	int next_inode; /* index of next free inode */
+	int protect; /* protection field */
+
 	int parent; /* inode of parent directory */
 	int permission;  /* permission */
-	int type; /* type of the file */
-
-	int protect; /* protection field */
+	char type; /* type of the file */
+	/***************************
+    	d 	Directory.
+    	l 	Symbolic link.
+    	- 	Regular file.
+	***************************/
 	int nlink; /* number of links to this file */
 	int size; /* numer of bytes in file */
 	int uid; /* owner’s user ID */
 	int gid; /* owner’s group ID */
+	/***************************
+	0: super user
+	-1: omitted argument
+	-2: nobody
+	https://en.wikipedia.org/wiki/User_identifier
+	***************************/
 	int ctime; /* change time */
 	int mtime; /* modification time */
 	int atime; /* access time */
@@ -28,3 +50,5 @@ struct inode {
 	int i2block; /* pointer to doubly indirect block */
 	int i3block; /* pointer to triply indirect block */
 };
+
+#endif
