6b3e94a7b2a1f86a08f92213c8c2639c0b0b074c
Ziting Shen <zitingshenbmc@gmail.com>
2018-05-08 16:37:32 -0400

    test i3

--- fs/tests.c
+++ fs/tests.c
@@ -310,7 +310,7 @@ void test_f_write_i2block() {
 
 	printf("%s\n", "Write an integer array of size 20000 to /design.txt.");
 	printf("%s\n", "The numbers are contiguous integers 1000 to 20999");
-	int size = 80000;
+	int size = 100000;
 	int arr[size];
 	for(int k = 0; k < size; k++) {
 		arr[k] = k+1000;
@@ -347,7 +347,7 @@ void test_f_write_i3block() {
 
 	printf("%s\n", "Write an integer array of size 20000 to /design.txt.");
 	printf("%s\n", "The numbers are contiguous integers 1000 to 20999");
-	int size = 100000;
+	int size = 2000000;
 	int arr[size];
 	for(int k = 0; k < size; k++) {
 		arr[k] = k+1000;
