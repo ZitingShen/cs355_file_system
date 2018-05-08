b20b97d9bc66da0483f0e02bd1996765c0bc9cb6
Ziting Shen <zitingshenbmc@gmail.com>
2018-05-08 14:22:09 -0700

    test i3

--- fs/fs.c
+++ fs/fs.c
@@ -1085,11 +1085,12 @@ int* which_to_find_free (size_t file_block, int block_num){
 	else if (block_num <=  N_DBLOCKS + POINTER_N * N_IBLOCKS + POINTER_N * POINTER_N){
 		int j = block_num - (N_DBLOCKS + POINTER_N * N_IBLOCKS); // block number relative to the start of i2block
 		int i = 0;
-		printf("%d %d\n", block_num, N_DBLOCKS + POINTER_N * N_IBLOCKS + POINTER_N * POINTER_N);
 		while(j >= POINTER_N) {
 			j-= POINTER_N;
 			i++;
 		}
+
+		printf("%d %d %d %d\n", block_num, i, j);
 
 		//j is now number of fully used 1st level block
 		//i is number of remaining data_block
@@ -1100,6 +1101,7 @@ int* which_to_find_free (size_t file_block, int block_num){
 				add_free[2] = 1;
 			}
 		}
+		printf("%d %d %d %d %d\n", block_num, add_free[0], add_free[1], add_free[2]);
 	}
 	/*if in triply indirect block*/
 	else if (block_num <=  N_DBLOCKS + POINTER_N * N_IBLOCKS + POINTER_N * POINTER_N 
