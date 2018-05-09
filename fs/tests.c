bab9f5e75f7e9905e1a760629c59bea459ded236
Ziting Shen <zitingshenbmc@gmail.com>
2018-05-08 18:00:02 -0700

    test valgrind

--- fs/tests.c
+++ fs/tests.c
@@ -791,13 +791,13 @@ int main() {
 	// test_f_opendir_root();
 	// printf("\n");
 
-	printf("%s\n", "test make directory under the root directory");
-	test_f_mkdir();
-	printf("\n");
-
-	// printf("%s\n", "test close root directory");
-	// test_f_closedir();
+	// printf("%s\n", "test make directory under the root directory");
+	// test_f_mkdir();
 	// printf("\n");
+
+	printf("%s\n", "test close root directory");
+	test_f_closedir();
+	printf("\n");
 
 	// printf("%s\n", "test read root directory");
 	// test_f_readdir();
