bc0afbe903675973b99fe30f517a1338d615a5cb
Ziting Shen <zshen@brynmawr.edu>
2018-05-09 21:26:51 -0400

    change test scale

--- fs/tests.c
+++ fs/tests.c
@@ -654,7 +654,7 @@ void test_f_remove_large() {
 
 	printf("%s\n", "Write an integer array of size 20000 to /design.txt.");
 	printf("%s\n", "The numbers are contiguous integers 1000 to 20999");
-	int size = 2000000;
+	int size = 20;
 	int *arr = malloc(sizeof(int)*size);
 	for(int k = 0; k < size; k++) {
 		arr[k] = k+1000;
